11a1dd5f672cc6ab52d48ea898812bb2f7ec3012
Mish Jude <145227966+mishjude@users.noreply.github.com>
2023-09-30 16:10:52 -0400

    starter files

---
+++ Player.cpp
@@ -0,0 +1,10 @@
+#include "Player.hpp"
+using namespace std;
+
+Player * Player_factory(const std::string &name, const std::string &strategy) {
+  assert(false);
+}
+
+std::ostream & operator<<(std::ostream &os, const Player &p) {
+  assert(false);
+}
